a460353457739248579bb619af91f599e9d83b53
monsmain <matinfarmani20@gmail.com>
2025-06-06 10:28:50 +0330

    Update hack.go

--- hack.go
+++ hack.go
@@ -1,3 +1,28 @@
+/* coded by @monsmain
+⚠️NOTE FA:
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+حق استفاده از این کد فقط برای مالک آن محفوظ است؛ هر گونه کپی‌برداری غیرمجاز با تمام قدرت قانون پیگیری خواهد شد
+
+⚠️NOTE EN:
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+The right to use this code is reserved only for its owner; any unauthorized copying will be pursued to the full force of the law.
+*/
 package main
 
 import (
@@ -112,10 +137,8 @@ func main() {
 	var passwords []string
 
 	if mode == 1 {
-		// AUTO ATTACK
 		passwords = loadPasswordsAuto()
 	} else {
-		// MANUAL ATTACK
 		passwords = loadPasswordsManual()
 	}
 
